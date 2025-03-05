84b0f59f8592667c208df8d6998ba3826ac5ef83
Sebastian garcia <sebastianag2002@gmail.com>
2025-03-05 17:10:20 -0500

    starting oncar changes

--- safety_controller/safety_controller.py
+++ safety_controller/safety_controller.py
@@ -18,27 +18,26 @@ class SafetyController(Node):
         super().__init__("safety_controller")
 
         self.declare_parameter("side", -1)
-
-        self.safety_subscriber = self.create_subscription(SafeDriveMsg, "/safety_topic", self.drive_msg_callback, 10)
-        self.safety_publisher = self.create_publisher(AckermannDriveStamped, "/drive", 10)
+        self.scan_subscriber = self.create_subscription(LaserScan, "/scan", self.laser_scan_callback, 10)
+        self.safety_subscriber = self.create_subscription(AckermannDriveStamped, "/vesc/low_level/ackermann_cmd", self.drive_msg_callback, 10)
+        self.safety_publisher = self.create_publisher(AckermannDriveStamped, "/vesc/low_level/input/safety", 10)
         self.SIDE = self.get_parameter('side').get_parameter_value().integer_value
 
         self.scan_length = None
+        self.max_speed = 1.5
+        self.speed = 1.5
 
-
-    def drive_msg_callback(self, msg):
-        """Processes the drive message."""
-        # get range slicing boundaries
+    def laser_scan_callback(self, msg):
+        # self.get_logger().info("Scanning")
         if not self.scan_length:
-            self.scan_length = len(msg.scan.ranges)
+            self.scan_length = len(msg.ranges)
             modifier = round(self.scan_length*(135/260))
             self.r1 = round(self.scan_length*(1 - (45/260)))
             self.r0 = self.r1 - modifier
             self.l0 = round(self.scan_length*(45/260))
             self.l1 = self.l0 + modifier
 
-        scan = msg.scan
-        drive_msg = msg.drive_msg.drive
+        scan = msg
         ranges = scan.ranges
 
         # hardcoded range values for sim testing
@@ -49,32 +48,42 @@ class SafetyController(Node):
         right_range = (self.r0, self.r1)
         left_range = (self.l0, self.l1)
 
-        HARD_STOP_BOUND = 0.1
-        SLOW_BOUND = 0.5
+        HARD_STOP_BOUND = 0.4
+        SLOW_BOUND = 0.8
 
         if self.SIDE == -1:
             rng = range(*right_range, 2)
         else:
             rng = range(*left_range, 2)
 
-        new_speed = drive_msg.speed
+        self.speed = self.max_speed
+
         for k in rng:
             average = 1/2 * (ranges[k] + ranges[k+1])
             if average < HARD_STOP_BOUND:
-                new_speed = 0.0
+                self.speed = 0.0
                 break
             if average < SLOW_BOUND:
-                new_speed *= .5
+                self.speed = .5 * self.max_speed
+        
+
+
+
+    def drive_msg_callback(self, msg):
+        """Processes the drive message."""
+        # get range slicing boundaries
 
         new_msg = AckermannDriveStamped()
-        new_drive_msg = drive_msg
-        new_drive_msg.speed = new_speed
-
-        new_msg.drive = new_drive_msg
-        new_msg.header = msg.drive_msg.header
+        new_msg.header = msg.header
+        new_msg.drive = msg.drive
+        new_msg.drive.speed = self.speed
 
         # Send drive
+        self.get_logger().info("self.speed: " + str(self.speed))
         self.safety_publisher.publish(new_msg)
+        # self.get_logger().info("Published drive message")
+
+
 
 
 def main():
